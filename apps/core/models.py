37af635e3029ed7c7a14f57d753a16af761b3d1d
z3kanya <romanovpain@mail.ru>
2025-12-21 16:33:12 +0600

    Создание класса User

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,7 +3,7 @@
 Здесь могут быть общие модели, расширение модели User и т.д.
 """
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 
 
 # Если нужна кастомная модель пользователя, раскомментируйте:
@@ -11,4 +11,14 @@ from django.contrib.auth.models import AbstractUser
 #     pass
 
 
-
+class User(AbstractUser):
+    # Модель пользователя
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user',
+        verbose_name='пользователь'
+        null=True,
+        blank=True
+    )
+    
