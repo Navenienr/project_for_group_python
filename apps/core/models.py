d9460550db5e217db85090ca3542f728da0f2801
z3kanya <romanovpain@mail.ru>
2025-12-21 17:48:40 +0600

    Создание класса админа

--- apps/core/models.py
+++ apps/core/models.py
@@ -26,11 +26,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
+    
     
 
 
 class Moderator(models.Model):
+    # Модель модератора
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -38,13 +39,67 @@ class Moderator(models.Model):
         verbose_name='Модератор'
     )
 
-    can_edit = models.BooleanField(default=True)
-    can_delete = models.BooleanField(default=True)
-    can_ban = models.BooleanField(default=False)
+    can_create_news = models.BooleanField(
+        verbose_name='Может создавать новости',
+        default=True
+    )
 
+    can_edit_news = models.BooleanField(
+        verbose_name='Может редактировать новости',
+        default=True
+    )
+    
+    
+    can_delete_news = models.BooleanField(
+        verbose_name='Может удалять новости',
+        default=True
+    )    
+
+    can_delete_comments = models.BooleanField(
+        verbose_name='Может удалять комментарии',
+        default=True
+    )
+
+    can_ban_1_day = models.BooleanField(
+        verbose_name='Может банить на 1 день',
+        default=True
+    )
     class Meta:
         verbose_name = 'Модератор'
         verbose_name_plural = 'Модераторы'
 
     def __str__(self):
         return f'Модератор {self.user.username}'
+    
+
+class AdminProfile(models.Model):
+    # Модель администратора
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='admin',
+        verbose_name='Админ'
+    )
+    
+    can_ban = models.BooleanField(
+        verbose_name='Может выдавать баны',
+        default=True
+    )
+    
+    can_manage_moderators = models.BooleanField(
+        verbose_name='Может управлять модераторами',
+        default=True
+    )
+    
+    can_view_statistics = models.BooleanField(
+        verbose_name='Может просматривать статистику',
+        default=True
+    )
+    
+
+    class Meta:
+        verbose_name = 'Администратор'
+        verbose_name_plural = 'Администраторы'
+
+    def __str__(self):
+        return f'Администратор {self.user.username}'
