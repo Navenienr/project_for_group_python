d678263dbc1fd0e77cb4baaa6de10e7100d8dd0c
z3kanya <romanovpain@mail.ru>
2026-02-24 22:18:24 +0600

    v2 тг бот

--- apps/bots/tgbot.py
+++ apps/bots/tgbot.py
@@ -1,12 +1,19 @@
+from django.utils import timezone
+import time
+from django.db import close_old_connections
 import asyncio
 from telegram import Bot
-from apps.core.models import User
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
 from apps.news.models import News
-import time
-from django.utils import timezone
+from apps.core.models import User
 
 
-TOKEN = '8553032782:AAEGpLhH8kZWIEWIvEPd1PL6kNBwOU-sLU4'
+TOKEN = '8733942341:AAGEpWvQnvvkMfaLLcO31TVRQ57RkpFj3cA'
 
 bot = Bot(token=TOKEN)
 
@@ -17,26 +24,28 @@ last_check = timezone.now()
 
 
 async def send_news(news):
-    # Рассылка новостей всем пользователям (у которых есть тг ID)
-    users = User.objects.filter(tg_username_id__isnull=False).exclude(tg_username_id='')
+    close_old_connections()
+
+    # Рассылка новостей всем пользователям (у которых есть тг username)
+    users = User.objects.filter(
+        tg_username__isnull=False).exclude(tg_username='')
 
     if not users:
-        print('Нет пользователей с тг ID')
+        print('Нет пользователей с тг username')
         return
-    
+
     text = f"""
-    {news.title}
-    {news.content[:200]}...
-    Ссылка: https://#домен#/news/{news.id}
-    """ # Ссылку нужно доработать
-    
+{news.title}
+{news.content[:200]}...
+Ссылка: https://gamesite.ru/news/{news.id}
+    """
 
     for user in users:
         try:
-            await bot.send_message(chat_id=user.tg_username_id, text=text)
+            await bot.send_message(chat_id=f"@{user.tg_username}", text=text)
         except Exception as e:
-            print(e)
-        
+            print(f"Ошибка @{user.tg_username}: {e}")
+
     print(f"Рассылка новости {news.id} завершена")
 
 
@@ -47,13 +56,14 @@ def check_for_new_news():
         created_at__gt=last_check,
         is_published=True
     )
-    
+
     for news in new_news:
         if news.id not in queue:
             queue.append(news.id)
-    
+
+
     last_check = timezone.now()
-    
+
 
 def sent_from_queue():
     check_for_new_news()
@@ -65,10 +75,16 @@ def sent_from_queue():
                 news = News.objects.get(id=news_id)
                 if news:
                     asyncio.run(send_news(news))
+            except News.DoesNotExist:
+                print(f"Новость {news_id} не найдена")
             except Exception as e:
-                print(e)
-            
+                print(f"Ошибка: {e}")
+
             time.sleep(120)
         else:
             time.sleep(300)
             check_for_new_news()
+
+
+if __name__ == "__main__":
+    sent_from_queue()
