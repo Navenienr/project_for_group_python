270043594be6511acb738bc47471866a4b27f9d7
z3kanya <romanovpain@mail.ru>
2026-03-02 21:44:00 +0600

    Обновлен urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
 
 
