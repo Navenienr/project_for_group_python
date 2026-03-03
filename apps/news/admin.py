c71f952d5c74cd036661adb7d191be3cf3b18778
z3kanya <romanovpain@mail.ru>
2026-03-03 20:47:16 +0600

    Добавлена админка для новостей

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from models import News
 # Register your models here.
 
 
-
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    search_fields = ['title']
+    readonly_fields = ['created_at']
