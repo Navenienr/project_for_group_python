fee1ca80099dd2d7d34c48c41fbf2d34e294475d
z3kanya <romanovpain@mail.ru>
2026-03-05 22:00:59 +0600

    справлены urls в news

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'news'
 
 router = DefaultRouter()
-router.register(r'news', views.NewsViewSet, basename='news')
+router.register(r'', views.NewsViewSet, basename='news')
 # router.register(r'articles', views.ArticleViewSet)
 # router.register(r'categories', views.CategoryViewSet)
 
@@ -17,6 +17,6 @@ router.register(r'news', views.NewsViewSet, basename='news')
 urlpatterns = [
     path('', include(router.urls)),
     # Ручная привязка комментариев
-    path('news/<int:news_pk>/comments/', views.CommentView.as_view({'get': 'list', 'post': 'create'})),
+    path('news/<int:news_pk>/comments/',views.CommentView.as_view({'get': 'list', 'post': 'create'})),
     path('news/<int:news_pk>/comments/<int:pk>/', views.CommentView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
